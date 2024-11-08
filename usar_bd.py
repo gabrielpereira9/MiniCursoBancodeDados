3baad1bfb6bb65ad48a854e28d912d6d3a9a340d
gabepereira9 <imtpereira44@gmail.com>
2024-11-08 20:34:11 -0300

    commit do projeto de banco de dados com python

---
+++ usar_bd.py
@@ -0,0 +1,14 @@
+import DAO
+from DAO import atualizar_usuario
+
+saida = DAO.inserir_usuario("gabriel", "email@email,com", "Per2022")
+#saida = DAO.inserir_usuario("amanda", "amanda@email,com", "1234")
+
+#saida =   DAO.buscar_usuario('gabriel')
+#saida = DAO.listar_usuarios()
+#saida = atualizar_usuario()
+
+
+
+#print(saida)
+
